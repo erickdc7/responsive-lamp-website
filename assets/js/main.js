07154355165ea15a59343202b10e5b854da25d2a
ErickDC <erickdiazc717@gmail.com>
2023-07-18 12:07:28 -0500

    scroll up function added

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,7 +97,14 @@ const toggleItem = (item) => {
 }
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+    this.scrollY >= 350
+        ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
