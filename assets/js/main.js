5b25074dc5ed4b7d1fcd5d1342b2f550947d8cd5
ErickDC <erickdiazc717@gmail.com>
2023-07-26 11:42:57 -0500

    features__image added

--- assets/js/main.js
+++ assets/js/main.js
@@ -168,5 +168,5 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home__content, .popular__container`)
 sr.reveal(`.home__image`, { origin: 'bottom' })
-sr.reveal(`.choose__image`, { origin: 'left' })
+sr.reveal(`.choose__image, .features__image`, { origin: 'left' })
 sr.reveal(`.choose__content`, { origin: 'right' })
