f759dffab223fb12f4376036bf5bf727bdd78ae3
ErickDC <erickdiazc717@gmail.com>
2023-07-26 11:39:38 -0500

    const sr created

--- assets/js/main.js
+++ assets/js/main.js
@@ -158,3 +158,10 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true // Animations repeat
+})
